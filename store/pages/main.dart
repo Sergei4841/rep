0cd8cd11375f75da04d88a11b022f358084da3cc
Сергей Н. <nozdrenkoserg@gmail.com>
2025-03-13 01:12:08 +0300

    Update main.dart

--- store/pages/main.dart
+++ store/pages/main.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:smartstore/design/colors.dart';
 import 'package:smartstore/pages/basket.dart';
+import 'package:smartstore/pages/consult.dart';
 import 'package:smartstore/pages/home.dart';
+import 'package:smartstore/pages/login.dart';
+import 'package:smartstore/pages/register.dart';
 
 void main() => runApp(
   MaterialApp(
     theme: ThemeData(
       primaryColor: primaryColor,
     ),
-    initialRoute: '/',
+    initialRoute: '/R',
     routes: {
       '/': (context) => Home(),
       '/B': (context) => const Basket(),
-    }
+      '/C': (context) => const Consult(),
+      '/R': (context) => Register(),
+      '/L': (context) => LoginPage(),
+    },
   ),
 );
