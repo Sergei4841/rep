9821c3b6a32523a15dde5eac2ead697f4eb5c410
Сергей Н. <nozdrenkoserg@gmail.com>
2025-05-14 23:18:00 +0300

    Update main.dart

--- store/pages/main.dart
+++ store/pages/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'package:smartstore/design/colors.dart';
-import 'package:smartstore/pages/basket.dart';
-import 'package:smartstore/pages/basketcheck.dart';
-import 'package:smartstore/pages/basketnocheck.dart';
-import 'package:smartstore/pages/consult.dart';
-import 'package:smartstore/pages/home.dart';
-import 'package:smartstore/pages/login.dart';
-import 'package:smartstore/pages/register.dart';
+import 'package:smartstore2/design/colors.dart';
+import 'package:smartstore2/pages/basket.dart';
+import 'package:smartstore2/pages/basketcheck.dart';
+import 'package:smartstore2/pages/basketnocheck.dart';
+import 'package:smartstore2/pages/consult.dart';
+import 'package:smartstore2/pages/home.dart';
+import 'package:smartstore2/pages/login.dart';
+import 'package:smartstore2/pages/register.dart';
+import 'package:smartstore2/pages/profilepage.dart';
 
 void main() => runApp(
-  MaterialApp(
-    theme: ThemeData(
-      primaryColor: primaryColor,
-    ),
-    initialRoute: '/R',
-    routes: {
-      '/H': (context) => Home(),
-      '/B': (context) => Basket(),
-      '/BC': (context) => BasketChecked(),
-      '/BNC': (context) => BasketNotChecked(),
-      '/C': (context) => const Consult(),
-      '/R': (context) => Register(),
-      '/L': (context) => LoginPage(),
-    },
-  ),
-);
+      MaterialApp(
+        theme: ThemeData(
+          primaryColor: primaryColor,
+        ),
+        initialRoute: '/C',
+        routes: {
+          '/H': (context) => Home(),
+          '/B': (context) => Basket(),
+          '/BC': (context) => BasketChecked(),
+          '/BNC': (context) => BasketNotChecked(),
+          '/C': (context) => const Consult(),
+          '/R': (context) => Register(),
+          '/L': (context) => LoginPage(),
+          'PP': (context) => ProfilePage(),
+        },
+      ),
+    );
